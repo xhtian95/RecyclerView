200115c081a6c5e0846d398cf1ce3f5de7529d4f
13152 <m15638250368@163.com>
2019-05-20 16:05:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/Hello.java
@@ -0,0 +1,5 @@
+package com.example.recyclerview;
+
+public class Hello {
+    String jingran;
+}
---
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+        FruitAdapter adapter = new FruitAdapter(fruitList);
+
+        recyclerView.setAdapter(adapter);
+       /* recyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+    }
+
+    public void initFruits() {
+        for (int i = 0; i < 10; i++) {
+            Fruit apple = new Fruit("Apple", R.mipmap.apple);
+            fruitList.add(apple);
+
+
+        }
+    }
+}
